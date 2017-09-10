cea997eb6f503c89bc9fe69c24c318efba798831
jfederico <jesus@123it.ca>
2017-09-10 19:56:07 +0000

    Added cleanblog theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   #get 'creative/index'
+  #get 'cleanblog/index'
   resources :projects do
     resources :logs
   end
 
   root 'creative#index'
+  #root 'cleanblog#index'
 end
